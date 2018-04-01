a81faa238dd1e241ffe0e6f866b254a93a5fa3a2
Maxim Kuzmenko <lifeofcows@gmail.com>
2018-04-01 14:59:57 -0400

    added certificate stuff

--- COMP4601-Assignment2/src/edu/carleton/comp4601/resources/CrawlerController.java
+++ COMP4601-Assignment2/src/edu/carleton/comp4601/resources/CrawlerController.java
@@ -1,10 +1,22 @@
 package edu.carleton.comp4601.resources;
 
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+
 import edu.uci.ics.crawler4j.crawler.CrawlConfig;
 import edu.uci.ics.crawler4j.crawler.CrawlController;
 import edu.uci.ics.crawler4j.fetcher.PageFetcher;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+import javax.net.ssl.X509TrustManager;
 
 public class CrawlerController {
 	CrawlController controller;
@@ -12,27 +24,73 @@ public class CrawlerController {
 	String crawlStorageFolder = "data/crawl/root";
 	public static String crawlBaseURL = "";
 	int maxDepthOfCrawling = 10;
+
+	public CrawlerController(String dir) throws Exception {
+		disableCertificates();
+		CrawlConfig config = new CrawlConfig();
+		config.setCrawlStorageFolder(crawlStorageFolder);
+		config.setMaxDepthOfCrawling(maxDepthOfCrawling);
+		config.setIncludeBinaryContentInCrawling(true);
+
+		PageFetcher pageFetcher = new PageFetcher(config);
+		RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+		RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+		controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
+		controller.addSeed("https://sikaman.dyndns.org/courses/4601/assignments/" + dir + "/users/");
+		controller.addSeed("https://sikaman.dyndns.org/courses/4601/assignments/" + dir + "/pages/");
+
+		crawlBaseURL = "https://sikaman.dyndns.org/courses/4601/assignments/" + dir + "/";
+		Database.getInstance();
+	}
+
+	public void crawl() {
+		Database.getInstance().clear();
+		controller.start(Crawler.class, numberOfCrawlers);
+	}
 	
-    public CrawlerController(String dir) throws Exception {
-        CrawlConfig config = new CrawlConfig();
-        config.setCrawlStorageFolder(crawlStorageFolder);
-        config.setMaxDepthOfCrawling(maxDepthOfCrawling);
-        config.setIncludeBinaryContentInCrawling(true);
-        
-        PageFetcher pageFetcher = new PageFetcher(config);
-        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
-        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
-        controller = new CrawlController(config, pageFetcher, robotstxtServer);
-        
-        controller.addSeed("https://sikaman.dyndns.org/courses/4601/assignments/" + dir + "/users/");
-        controller.addSeed("https://sikaman.dyndns.org/courses/4601/assignments/" + dir + "/pages/");
-        
-        crawlBaseURL = "https://sikaman.dyndns.org/courses/4601/assignments/" + dir + "/";
-        Database.getInstance();
-    }
-    
-    public void crawl() {
-    		Database.getInstance().clear();
-    		controller.start(Crawler.class, numberOfCrawlers);
-    }
+	public void disableCertificates() {
+		TrustManager[] trustAllCerts = new TrustManager[] {
+				new X509TrustManager() {
+					public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+						return null;
+					}
+					@Override
+					public void checkClientTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+						// TODO Auto-generated method stub
+
+					}
+					@Override
+					public void checkServerTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+						// TODO Auto-generated method stub
+
+					}
+				}
+		};
+
+		// Install the all-trusting trust manager
+		SSLContext sc = null;
+		try {
+			sc = SSLContext.getInstance("SSL");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			sc.init(null, trustAllCerts, new java.security.SecureRandom());
+		} catch (KeyManagementException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+		// Create all-trusting host name verifier
+		HostnameVerifier allHostsValid = new HostnameVerifier() {
+			public boolean verify(String hostname, SSLSession session) {
+				return true;
+			}
+		};
+
+		HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+	}
 }
